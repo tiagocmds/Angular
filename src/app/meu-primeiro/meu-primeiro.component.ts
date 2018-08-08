a0fbe65d50cf24d4814c99bbc935ca7a8c686a72
Tiago Camargo da Silva <tiago.silva@agrario.gov.br>
2018-08-08 13:53:36 -0300

    Aula 5

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+    @Component({
+        selector:'meu-primeiro-component',
+        template: `
+            <p>Meu primeiro component com Angular 2!</p>
+        `
+    })
+export class MeuPrimeiroComponent
+{
+
+}
